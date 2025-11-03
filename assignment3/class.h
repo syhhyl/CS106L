dd56a4d5596db52af5e1b690712ede80187157ea
syhhyl <syhhyl926@126.com>
2025-11-04 00:15:05 +0800

    a3 finish

---
+++ assignment3/class.h
@@ -0,0 +1,10 @@
+
+// Point
+struct Point {
+  double x{0}, y{0}; 
+  constexpr Point() = default;
+  constexpr Point(double x_, double y_) : x(x_), y(y_) {}
+  friend
+  
+
+};
--- assignment3/sandbox.cpp
+++ assignment3/sandbox.cpp
@@ -3,6 +3,8 @@
  * Created by Fabio Ibanez with modifications by Jacob Roberts-Baca.
  */
 
+#include "class.h"
+
 void sandbox() {
   // STUDENT TODO: Construct an instance of your class!
 }
